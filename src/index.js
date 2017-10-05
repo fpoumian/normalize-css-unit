dc7616ceef84f32dbe5d662aea8e8cdaa95f2f24
Fernando Poumián <fpoumian@gmail.com>
2017-10-04 20:17:47 -0600

    feat(*): Add main function and tests.
    Add main function to index.js and tests to index.test.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-/* @flow */
-export default function normalizeCssUnit(input: string) {
-  return input ? `👉 ${input} 👈` : 'No args passed!';
+import isNumber from 'lodash.isnumber';
+import isString from 'lodash.isstring';
+
+export default function normalizeCSSUnit(unitValue) {
+  if (!isNumber(unitValue) && !isString(unitValue)) {
+    throw new TypeError(
+      `You need to pass a number or a string as an argument. ${unitValue
+        .constructor.name} provided.`
+    );
+  }
+  return isNumber(unitValue) ? `${unitValue}px` : unitValue.trim();
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,50 @@
-import normalizeCssUnit from './index';
+import normalizeCSSUnit from './index';
 
 describe('index.js', () => {
-  it('should say something', () => {
-    expect(normalizeCssUnit('🐰')).toEqual('👉 🐰 👈');
-    expect(normalizeCssUnit()).toEqual('No args passed!');
+  describe('given a string as a value', () => {
+    const result = normalizeCSSUnit('10rem');
+
+    it('should return the same string as is', () => {
+      expect(result).toBe('10rem');
+    });
+
+    describe('given the string has extra blank spaces', () => {
+      it('should trim the string', () => {
+        expect(normalizeCSSUnit(' 100px ')).toBe('100px');
+      });
+    });
+  });
+  describe('given an integer as a value', () => {
+    const result = normalizeCSSUnit(200);
+
+    it('should return the value as pixels', () => {
+      expect(result).toBe('200px');
+    });
+  });
+  describe('given a number with decimals as a value', () => {
+    const result = normalizeCSSUnit(20.5);
+
+    it('should return the value as pixels', () => {
+      expect(result).toBe('20.5px');
+    });
+  });
+  describe('given an invalid argument was passed', () => {
+    it('should throw a TypeError', () => {
+      expect(() => {
+        normalizeCSSUnit({});
+      }).toThrowError(TypeError);
+    });
+    it('should throw an error with correct message', () => {
+      expect(() => {
+        normalizeCSSUnit([10]);
+      }).toThrowError(
+        'You need to pass a number or a string as an argument. Array provided.'
+      );
+      expect(() => {
+        normalizeCSSUnit({});
+      }).toThrowError(
+        'You need to pass a number or a string as an argument. Object provided.'
+      );
+    });
   });
 });
